73392cf063e77b347f8701254b6e1a90a2414234
Dilan-993 <grysha1021@gmail.com>
2026-06-26 10:45:18 +0300

    второй комментарий

---
+++ parsing_quotes.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com/" # сохраняем ссылку интернет страницы
+response = requests.get(url) # скачиваю страницу по указыной ссылки
+
+soup = BeautifulSoup(response.text, "html.parser") # это раскладывание кода(html) по полочкам
+quotes = soup.find_all("div", class_="quote") # находим нужные элементы из разложенной информации
+for quote in quotes: # беру отдельную цитату среди всех цитат
+    text = quote.find("span", class_="text").text
+    author = quote.find("small", class_="author").text
+
+    print(text)
+    print("автор",author)
